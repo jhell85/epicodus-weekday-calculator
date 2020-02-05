280f798bd2d69cc19929c0613eb7402099a613ea
Brandan Sayarath, Joshua Hellman and Nick Gallo <nick@>
2020-02-05 14:10:22 -0800

    add functionality to succesfully test for day of the week

--- __tests__/weekday-calculator.test.js
+++ __tests__/weekday-calculator.test.js
@@ -3,8 +3,13 @@ import { dayCalculator } from './../src/dayOfDate.js';
 describe('dayCalculator(year, month, day)', () => { 
   
   test('should determine if a date is not valid', () => {
-    let userDate = new dayCalculator(2020, 12, 31);
-    expect(userDate.checkValidDate()).toEqual(true);
+    let userDate = new dayCalculator(2020, 12, 32);
+    expect(userDate.checkValidDate()).toEqual(false);
     });
+
+  test('should return the day of the week for past date', () => {
+    let pastDate = new dayCalculator(1985, 6, 18)
+    expect(pastDate.checkValidDate()).toEqual("Tuesday")
+  })
 });
 
