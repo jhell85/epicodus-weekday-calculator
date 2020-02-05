41dc8f6e05672c196058c336036492c9e4fcc9c8
Brandan Sayarath, Joshua Hellman and Nick Gallo <nick@>
2020-02-05 14:11:03 -0800

    return string for day of week based on .getDay 0-6day of week array

--- src/dayOfDate.js
+++ src/dayOfDate.js
@@ -7,9 +7,39 @@ export function dayCalculator(year, month, day) {
 
 dayCalculator.prototype.checkValidDate = function() {
   if ((this.day <= 31 && this.day >= 1) && (this.month <= 12 && this.month >= 1)) {
-    return true;
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    let date = new Date(`${this.year}, ${this.month}, ${this.day}`)
+    let dayResult = weekday[date.getDay()]
+    return dayResult
   } else {
+    let date = new Date(`${this.year}, ${this.month}, ${this.day}`)
+    console.log(`${this.year} ${this.month} ${this.day}`)
+    console.log(date)
     return false;
   }
 }
 
+
+
+// dayCalculator.prototype.returnDayOfWeek = function () {
+
+// var weekday = new Array(7);
+// weekday[0] = "Sunday";
+// weekday[1] = "Monday";
+// weekday[2] = "Tuesday";
+// weekday[3] = "Wednesday";
+// weekday[4] = "Thursday";
+// weekday[5] = "Friday";
+// weekday[6] = "Saturday";
+
+//  return weekday[this.getDay()];
+
+// }
